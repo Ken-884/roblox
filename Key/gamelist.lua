e00e4efee85b7a44d705ebaa2d10b3ff0e56483d
Mentos4 <zandrixjanpongos0104@gmail.com>
2025-08-14 16:09:30 +0800

    Create gamelist.lua

---
+++ Key/gamelist.lua
@@ -0,0 +1,17 @@
+
+local Games = {
+    -- Swordburst 3 floors
+    ["4093155512"] = "https://raw.githubusercontent.com/Mentos4/Roblox/refs/heads/main/Key/Key_Swordburst3.txt",
+    -- Dungeon Heroes
+    ["7546582051"] = "https://raw.githubusercontent.com/Mentos4/Roblox/refs/heads/main/Key/Key_DungeonHeroes.txt",
+    -- Anime Eternal
+    ["7882829745"] = "https://raw.githubusercontent.com/Mentos4/Roblox/refs/heads/main/Key/Key_AnimeEternal.txt",
+    -- Hypershot
+    ["5995470825"] = "https://raw.githubusercontent.com/Mentos4/Roblox/refs/heads/main/Key/Key_HyperShot.txt",
+    -- Build an Island
+    ["7541395924"] = "https://raw.githubusercontent.com/Mentos4/Roblox/refs/heads/main/Key/Key_BuildanIsland.txt",
+    -- Blue Heater 2
+    ["5803093656"] = "https://raw.githubusercontent.com/Mentos4/Roblox/refs/heads/main/Key/Key_BlueHeater2.txt"
+}
+
+return Games
