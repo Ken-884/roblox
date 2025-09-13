64284e3b9efd235caa785a768e1f91480c739d96
Ken-884 <zandrixpongos11@gmail.com>
2025-09-14 02:41:24 +0800

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,22 +1,22 @@
 local Games = {
-    -- Anime Eternal
-    ["7882829745"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_AnimeEternal.lua",
-    -- Anime Ranger X
-    ["6884266247"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_AnimeRangerX.lua",
-    -- Arise Crossover
-    ["7074860883"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_AriseCrossover.lua",
-    -- Blue Heater 2
-    ["5803093656"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_BlueHeater2.lua",
-    -- Build an Island
-    ["7541395924"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_BuildanIsland.lua",
+    -- Swordburst 3 floors
+    ["4093155512"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_AnimeEternal.lua",
     -- Dungeon Heroes
     ["7546582051"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_DungeonHeroes.lua",
+    -- Anime Eternal
+    ["7882829745"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_AnimeEternal.lua",
     -- Hypershot
     ["5995470825"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_Hypershot.lua",
+    -- Build an Island
+    ["7541395924"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_BuildanIsland.lua",
+    -- Blue Heater 2
+    ["5803093656"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_BlueHeater2.lua",
+    -- Arise Crossover
+    ["7074860883"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_AriseCrossover.lua",
+    -- Anime Ranger X
+    ["6884266247"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_AnimeRangerX.lua",
     -- Restaurant Tycoon 3
     ["7094518649"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_RestaurantTycoon3.lua",
-    -- Swordburst 3 floors
-    ["4093155512"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_Swordburst3.lua",
 }
 
 return Games
