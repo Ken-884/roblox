dde520ae8737e0cda6ba2db27040f826e34ac887
Ken-884 <zandrixpongos11@gmail.com>
2025-09-27 05:38:48 +0800

    Update subgamelist.lua

--- ScriptNoExpire/subgamelist.lua
+++ ScriptNoExpire/subgamelist.lua
@@ -1,23 +1,22 @@
 local Games = {
     -- Swordburst 3 floors
-    ["4093155512"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_Swordburst3.lua",
+    ["4093155512"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/Swordburst3Final.lua",
     -- Dungeon Heroes
-    ["7546582051"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_DungeonHeroes.lua",
+    ["7546582051"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/DungeonHeroesFinal.lua",
     -- Anime Eternal
-    ["7882829745"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_AnimeEternal.lua",
+    ["7882829745"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/AnimeEternalFinal.lua",
     -- Build an Island
-    ["7541395924"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_BuildanIsland.lua",
+    ["7541395924"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/BuildanIslandFinal.lua",
     -- Blue Heater 2
-    ["5803093656"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_BlueHeater2.lua",
+    ["5803093656"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/BlueHeater2Final.lua",
     -- Arise Crossover
-    ["7074860883"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_AriseCrossover.lua",
+    ["7074860883"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/AriseCrossoverFinal.lua",
     -- Anime Ranger X
-    ["6884266247"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_AnimeRangerX.lua",
+    ["6884266247"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/AnimeRangerXFinal.lua",
     -- Restaurant Tycoon 3
-    ["7094518649"] = "https://raw.githubusercontent.com/Ken-884/Roblox/refs/heads/main/Script/Script_RestaurantTycoon3.lua",
+    ["7094518649"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/RestaurantTycoon3Final.lua",
     -- Build a Zoo
-    ["8066283370"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_BuildAZoo.lua"
+    ["8066283370"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/ScriptNoExpire/Script/BuildAZooFinal.lua"
 }
-
 
 return Games
