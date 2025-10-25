0e2954b9e00a7adc8817e01d54d16bd8a8697a56
Ken-884 <zandrixpongos11@gmail.com>
2025-10-25 11:47:31 +0800

    Update gamelist.lua

--- premium/gamelist.lua
+++ premium/gamelist.lua
@@ -22,11 +22,12 @@ local Games = {
     -- Fish It
     ["6701277882"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_FishIt.lua",
     -- Plant vs Brainrot
-    ["8316902627"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_PlantvsBrainrot.lua"
+    ["8316902627"] = "https://raw.githubusercontent.com/Ken-884/roblox/refs/heads/main/Script/Script_PlantvsBrainrot.lua",
 }
 
 
 return Games
+
 
 
 
