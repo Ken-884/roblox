bf3c21dc03fc97a056d59d89aae0d6175e94c6a9
Ken-884 <zandrixpongos11@gmail.com>
2026-03-07 21:03:28 +0800

    Update discontinued.lua

--- discontinued.lua
+++ discontinued.lua
@@ -1,6 +1,10 @@
 -- List of discontinued game IDs
 local Discontinued = {
     ["5995470825"] = true, -- Hypershot
+    ["8328640632"] = true, -- Fish it
+    ["6867859535"] = true, -- RE:XL
+    ["7074860883"] = true, -- Arise Crossover
+    ["8820222330"] = true, -- Dig and Hatch a Brainrot
     -- Add more discontinued game IDs here
 }
 
